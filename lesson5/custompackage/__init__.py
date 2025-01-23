a793a4e800eed199cd66cf0f3318a21c51b9a269
zarytsko <zaritskiy.alexey@gmail.com>
2025-01-23 22:24:00 +0200

    Lesson 5-6: Modules and conditionals

---
+++ lesson5/custompackage/__init__.py
@@ -0,0 +1,6 @@
+from .meowing_module import meow
+
+print("Доброго дня! Дякую що імпортували мій пакет!")
+__all__ = ["greeting_module", "meowing_module", "meow"]
+
+
---
+++ lesson5/custompackage/greeting_module.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(f"Привіт, {name}!")
+
+
+def add(a, b):
+    return a + b
+
+
+PI = 3.14159
+
+very_secret_numer = 42
