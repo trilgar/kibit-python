a0491d9be45dc521777e03f53fe90b0682e678e6
zarytsko <oleksii.zarytskyi@vmedia.ca>
2025-04-30 12:49:58 +0300

    Numpy

--- lesson16/task11.py
+++ lesson16/task11.py
@@ -22,3 +22,49 @@
 
     Додатково перевизначте метод __repr__, щоб зручно виводити об'єкти у консоль.
 """
+
+class Potion:
+    def __init__(self, power, name):
+        self.power = power
+        self.name = name
+
+    def __str__(self):
+        return f"Strength of the {self.name} potion is {self.power}"
+
+    def __add__(self, other):
+        if not isinstance(other, Potion):
+            print("Cannot add other type than Potion")
+            return self
+        print("Adding two potions")
+        name  = self.name + " " + other.name
+        power = self.power + other.power
+        return Potion(power, name)
+
+    def __sub__(self, other):
+        if not isinstance(other, Potion):
+            print("Cannot substract other type than Potion")
+            return self
+        print("Substracting two potions")
+        name  = self.name + " " + other.name
+        power = self.power - other.power
+        if power < 0:
+            power = 0
+        return Potion(power, name)
+
+    def __mul__(self, other):
+        if not isinstance(other, Potion):
+            print("Cannot multiply other type than Potion")
+            return self
+        print("Multiplying two potions")
+        name = self.name + " " + other.name
+        power = self.power * other.power
+        return Potion(power, name)
+
+
+strength_potion = Potion(10, "Strong")
+
+weakness_potion = Potion(0, "Weakness")
+
+small_strength_potion = Potion(3, "small strength")
+
+print(strength_potion * weakness_potion)
