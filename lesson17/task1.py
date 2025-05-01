11df1517fec375b724f48538a237d409f49ea0f6
zarytsko <oleksii.zarytskyi@vmedia.ca>
2025-05-01 21:43:21 +0300

    numpy

--- lesson17/task1.py
+++ lesson17/task1.py
@@ -13,4 +13,45 @@
 🔧 Підказка:
 
     Використовуйте time.time() або time.perf_counter().
+
+    for i in range(n)
+
+    def func(*args, **kwargs):
+        hello_world(*args, **kwargs)
+
+    def hello_world(name, surname):
+        pass
+    def hello(name, surname):
+        pass
+
+    func(1,3,4,5,5,6,67,77)
+    func(surname = "Zarytskyi", name = "Oleksii")
+
+
 """
+import time
+
+def timed(f):
+    def time_wrapped(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = f(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f"Func took time: {end_time-start_time}")
+        return result
+    return time_wrapped
+
+@timed
+def loop(n):
+    for i in range(n):
+        print(1)
+
+
+@timed
+def count_hello(name, surname):
+    for i in range(1000000):
+        print("Calculating hello")
+
+    return f"Hello, {name}, {surname}"
+
+loop(1000000)
+count_hello("Oleg", "Mongol")
--- lesson17/task2.py
+++ lesson17/task2.py
@@ -15,4 +15,29 @@
     greet(name), яка повертає рядок "Привіт, {name}!"
 
     multiply(a, b), яка повертає добуток двох чисел.
+
+
+    func.__name__
 """
+
+def logged(f):
+    def qwe(*args, **kwargs):
+        print(f"Назва функції: {f.__name__} ")
+        print(f"Аргументи з якими її викликали: {args, kwargs}")
+        result = f(*args, **kwargs)
+        print(result)
+        return result
+    return qwe
+
+@logged
+def loop(n):
+    for i in range(n):
+        print(1)
+@timed
+@logged
+def count_hello(name, surname):
+    for i in range(2):
+        print("Calculating hello")
+
+loop(2)
+count_hello("Oleg", surname = "Mongol")
