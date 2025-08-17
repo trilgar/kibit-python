065090e53b403ec5a3cae922a70e8d66392b23bf
zarytsko <oleksii.zarytskyi@vmedia.ca>
2025-08-17 13:15:41 +0300

    hadnling dates and saving charts

---
+++ lesson26_data_visualization/my_first_chart.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# код у ваших комірках (приклад для демо)
+import matplotlib.pyplot as plt
+
+plt.axis([0, 5, 0, 20])
+plt.title('My first plot')
+plt.xlabel('Counting')
+plt.ylabel('Square values')
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+print("hello world")
